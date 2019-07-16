757ccb8958fce9c2a3a7d25c7ee07b769a73cbf3
Hexin <hexium310@gmail.com>
2019-07-16 17:13:43 +0900

    Add paths in tsconfig.json

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,11 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions: { setWebpackConfig } }) => {
+  setWebpackConfig({
+    resolve: {
+      plugins: [
+        new TsconfigPathsPlugin(),
+      ],
+    },
+  });
+};
