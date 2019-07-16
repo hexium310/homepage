185c79a95d9ce1b628fc43cd4b18b5c4e1ec9f58
Hexin <hexium310@gmail.com>
2019-07-17 04:28:25 +0900

    Add index page

---
+++ src/components/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About: React.FunctionComponent = () => {
+  return (
+    <div>
+      Name: へきしん
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import About from 'components/About';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <>
+      <About />
+    </>
+  );
+};
+
+export default App;
