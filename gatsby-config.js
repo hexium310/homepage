ff5a775bcf5a9e28a6101866734c4a1df9ace991
Hexin <hexium310@gmail.com>
2019-07-19 04:32:57 +0900

    Specify loading tsconfig.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
     // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    'gatsby-plugin-ts-loader',
     {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,33 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-exports.onCreateWebpackConfig = ({ actions: { setWebpackConfig } }) => {
+const tsconfigFile = process.env.NODE_ENV === 'production' ? 'tsconfig.production.json' : 'tsconfig.json';
+
+exports.resolvableExtensions = () => [".ts", ".tsx", ".js", ".jsx"];
+
+exports.onCreateWebpackConfig = ({
+  loaders,
+  actions: {
+    setWebpackConfig,
+  },
+}) => {
   setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: [
+            loaders.js(),
+            {
+              loader: 'ts-loader',
+              options: {
+                configFile: tsconfigFile,
+              },
+            },
+          ],
+        },
+      ],
+    },
     resolve: {
       plugins: [
         new TsconfigPathsPlugin(),
